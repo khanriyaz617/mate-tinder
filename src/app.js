06257e014a8e17aabd163608f6965fee4857a340
Shree Riyaz <khanriyaz617@gmail.com>
2025-02-16 22:38:43 +0530

    Multiple route-handlers exercises

--- src/app.js
+++ src/app.js
@@ -2,15 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// app.get("/user", (req, res) => {
+//   console.log("It is a Route Handler function in expressjs.");
 
-app.get("/user", (req, res)=>{
-    res.send("Welcome Me! I am coming from server world !")
-})
+//   res.send("Welcome Me! I am coming from server world !");
+// });
 
-app.post("/user", (req, res)=>{
-    res.send("Data saved successfully.");
-})
+// app.post("/user", (req, res) => {
+//   res.send("Data saved successfully.");
+// });
 
-app.listen(7777, ()=>{
-    console.log('App successfully started on port 7777');
-})
+app.use("/user", [
+  (req, res, next) => {
+    //   res.send("I am route handler callback function 1.");
+    next();
+  },
+  (req, res, next) => {
+    res.send("I am route handler callback function 2.");
+    s;
+  },
+]);
+
+app.listen(7777, () => {
+  console.log("App successfully started on port 7777");
+});
